b00dced6c694d7be5f08a46f67ae77a50e7ef9be
Jason Dusek <jason.dusek@gmail.com>
2011-11-01 07:04:50 +0000

    Options parsing draft but no subcommands :(

---
+++ System/Posix/ARX/Options.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE OverloadedStrings
+           , TupleSections
+           , StandaloneDeriving #-}
+
+module System.Posix.ARX.Options where
+
+import Control.Applicative
+import Control.Monad
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as Bytes
+import qualified Data.ByteString.Char8 as Char8
+import Data.List
+import Data.Word
+import Text.Parsec hiding (satisfy, (<|>))
+
+import qualified Data.Attoparsec
+
+import System.Posix.ARX.CLTokens (Class(..))
+import qualified System.Posix.ARX.CLTokens as CLTokens
+import qualified System.Posix.ARX.Sh as Sh
+import System.Posix.ARX.Tar
+
+
+{-| Finds a run of two or more slashes; if there is more than one such run,
+    returns the longest one.
+ -}
+longestSlashRun             ::  [ByteString] -> Maybe ByteString
+longestSlashRun              =  foldl' f Nothing
+ where
+  f Nothing b  | slashes b   =  guard (Bytes.length b > 1) >> Just b
+               | otherwise   =  Nothing
+  f (Just a) b | slashes b   =  Just (longest a b)
+               | otherwise   =  Just a
+  slashes                    =  Char8.all (== '/')
+  longest a b                =  if Bytes.length b > Bytes.length a then b
+                                                                   else a
+
+
+blockSize                   ::  ArgsParser Word
+blockSize                    =  do arg "-b"
+                                   CLTokens.sizeBounded <@> tokCL Size
+
+outputFile                  ::  ArgsParser IOStream
+outputFile                   =  arg "-o" >> ioStream
+
+ioStream                    ::  ArgsParser IOStream
+ioStream                     =  STDIO <$  tokCL Dash
+                            <|> Path  <$> tokCL QualifiedPath
+
+tar                         ::  ArgsParser (Tar, IOStream)
+tar = (,) <$> (TAR <$ arg "-tar" <|> TBZ <$ arg "-tbz" <|> TGZ <$ arg "-tgz")
+          <*> ioStream
+
+rm                          ::  ArgsParser (Bool, Bool)
+rm  =   (True,  False) <$ arg "-rm0"  <|>  (False, True) <$ arg "-rm1"
+   <|>  (False, False) <$ arg "-rm!"  <|>  (True,  True) <$ arg "-rm_"
+
+run                         ::  ArgsParser IOStream
+run                          =  arg "-run" >> ioStream
+
+env                         ::  ArgsParser (Sh.Var, Sh.Val)
+env                          =  do
+  (var, assignment)         <-  Char8.break (== '=') <$> tokCL EnvBinding
+  case (,) <$> Sh.var var <*> Sh.val (Bytes.drop 1 assignment) of
+    Nothing                 ->  mzero
+    Just x                  ->  return x
+
+cmd                         ::  ArgsParser [ByteString]
+cmd bars = arg bars >> manyTill anyArg (eof <|> () <$ arg bars)
+
+
+{-| A byte-oriented store that can be read from or written to in a streaming
+    fashion.
+ -}
+data IOStream                =  STDIO | Path ByteString
+deriving instance Eq IOStream
+deriving instance Ord IOStream
+deriving instance Show IOStream
+
+
+type ArgsParser              =  Parsec [ByteString] ()
+
+satisfy                     ::  (ByteString -> Bool) -> ArgsParser ByteString
+satisfy p                    =  argPrim test
+ where
+  test b                     =  guard (p b) >> Just b
+
+anyArg                      ::  ArgsParser ByteString
+anyArg                       =  argPrim Just
+
+arg                         ::  ByteString -> ArgsParser ByteString
+arg b                        =  satisfy (== b)
+
+argPrim                     ::  (ByteString -> Maybe t) -> ArgsParser t
+argPrim                      =  tokenPrim show next
+ where
+  next pos _ _               =  incSourceLine pos 1
+
+(<@>) :: Data.Attoparsec.Parser t -> ArgsParser ByteString -> ArgsParser t
+atto <@> parsec              =  do
+  res                       <-  Data.Attoparsec.parseOnly atto <$> parsec
+  case res of Left _        ->  mzero
+              Right x       ->  return x
+infixl 4 <@>
+
+tokCL                       ::  Class -> ArgsParser ByteString
+tokCL tokenClass             =  satisfy (CLTokens.match tokenClass)
+
