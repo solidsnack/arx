c0354ef2c66dda09257000bb6e74909d96f61e58
Jason Dusek <jason.dusek@gmail.com>
2011-11-07 05:12:20 +0000

    Support for file magic.

--- System/Posix/ARX/Tar.hs
+++ System/Posix/ARX/Tar.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE OverloadedStrings
+           , StandaloneDeriving #-}
 
 module System.Posix.ARX.Tar where
+
+import Prelude hiding (drop, take)
+import Data.ByteString.Lazy.Char8
 
 
 {-| Handled styles of Tar archive.
@@ -8,4 +13,19 @@ data Tar                     =  TAR | TGZ | TBZ
 deriving instance Eq Tar
 deriving instance Ord Tar
 deriving instance Show Tar
+
+
+{-| Scan a lazy ByteString for file magic.
+ -}
+magic                       ::  ByteString -> Maybe Tar
+magic b | bzMagic b          =  Just TBZ
+        | gzMagic b          =  Just TGZ
+        | tarMagic b         =  Just TAR
+        | otherwise          =  Nothing
+
+bzMagic                      =  (== "BZh")      . take 3
+
+gzMagic                      =  (== "\x1F\x8b") . take 2
+
+tarMagic                     =  (== "ustar")    . take 5 . drop 257
 
