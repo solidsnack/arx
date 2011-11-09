d327360f21d5e0656cc7c9917adde422e8a7cb0e
Jason Dusek <jason.dusek@gmail.com>
2011-11-09 19:28:34 +0000

    Remove fancy man/tmp stuff to build on Macs.

--- arx.hs
+++ arx.hs
@@ -10,7 +10,6 @@ import System.IO
 import System.Process
 
 import Data.FileEmbed
-import System.Unix.Directory
 
 import qualified System.Posix.ARX.CLI
 
@@ -25,26 +24,8 @@ main                         =  do
 checkHelp = any (`elem` ["-h", "-?", "--help"]) <$> getArgs
 
 
-man                          =  $(embedFile "./docs/blessed/arx.man")
-
 txt                          =  $(embedFile "./docs/blessed/arx.txt")
 
-sendHelp                     =  do
-  manResult                 <-  throughMan man
-  case manResult of
-    Just exitCode           ->  exitWith exitCode
-    Nothing                 ->  do Data.ByteString.putStr txt
+sendHelp                     =  do Data.ByteString.putStr txt
                                    exitSuccess
-
-{-| Runs man interactively on the input file.
- -}
-throughMan                  ::  ByteString -> IO (Maybe ExitCode)
-throughMan manText           =  do
-  exit                      <-  system "which man"
-  if exit /= ExitSuccess
-    then  return Nothing
-    else  Just <$> withTemporaryDirectory "arx.man." runManInTMP
- where
-  runManInTMP dir = do Data.ByteString.writeFile (dir ++ "/arx.man") manText
-                       system (unwords ["cd", dir, "&&", "man ./arx.man"])
 
