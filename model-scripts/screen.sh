080ec02ff226ab77630c51bc6ceb94de226cdfed
Jason Dusek <jason.dusek@gmail.com>
2012-01-15 23:22:13 +0000

    Make configuration parameterized.

--- model-scripts/screen.sh
+++ model-scripts/screen.sh
@@ -2,14 +2,20 @@ set -e -u
 
 #tag="`basename "$1"`"
 tag=tmpx
-screen_() {
+
+screen_S() {
   screen -S "$tag" "$@"
-  sleep 2
 }
 
-screen_ -dm -ln sleep 10
-# Set up placeholder window and free window 0; then apply configuration.
-screen_      -X eval 'screen 1 sleep 5' 'select 0' kill 'zombie ko'
-screen_      -X screen 0 "$1"
-screen_      -X eval 'select 1' kill
+screen_eval() {
+  cmd="$1" ; shift
+  screen_S -dm -ln sleep 2
+  # Set up placeholder window and free window 0; then apply settings.
+  screen_S      -X eval 'screen 1 sleep 1' 'select 0' kill "$@"
+  # Launch command.
+  screen_S      -X screen 0 "$cmd"
+  screen_S      -X eval 'select 1' kill
+}
+
+screen_eval "$1" 'zombie ko'
 echo "$tag"
